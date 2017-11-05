52c30de59a199b9e86c6d8d0f18ae66c5b91ddaa
osirisz <earthstroming@hotmail.com>
2017-11-05 21:18:56 +0700

    getCurrentLocation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, ToastController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Marker, MarkerOptions, LatLng } from '@ionic-native/google-maps';
 
@@ -13,54 +13,74 @@ export class HomePage {
 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
-
-  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+  loading: any;
+  constructor(public navCtrl: NavController, public geolocation: Geolocation, public loadingCtrl: LoadingController, public toastCtrl: ToastController) {
 
   }
 
   ionViewDidLoad() {
     this.loadMap();
   }
+  getCurrentPosition() {
+
+    let locationOptions = { timeout: 10000, enableHighAccuracy: true };
+
+    this.geolocation.getCurrentPosition(locationOptions).then(
+      (position) => {
+        let myPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        let options = {
+          center: myPos,
+          zoom: 14
+        };
+        this.map.setOptions(options);
+        this.addMarker2(myPos, "Mein Standort!");
+      },
+      (error) => {
+        this.loading.dismiss().then(() => {
+          this.showToast('Location not found. Please enable your GPS!');
+
+          console.log(error);
+        });
+      }
+    )
+  }
+  showToast(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
+  }
 
    loadMap() {
+     let latLng = new google.maps.LatLng(13.6510942, 100.4939486);
 
-     this.geolocation.getCurrentPosition().then(async (position) => {
+     let mapOptions = {
+       center: latLng,
+       zoom: 15,
+       mapTypeId: google.maps.MapTypeId.ROADMAP
+     }
 
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-
-      this.map = await new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-      this.addMarker(this.map)
-    }, (err) => {
-      console.log(err);
-    });
+     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+     this.addMarker(this.map)
 
   }
- 
+   addMarker2(position, content) {
+     let marker = new google.maps.Marker({
+       map: this.map,
+       animation: google.maps.Animation.DROP,
+       position: position
+     });
+
+     this.addInfoWindow(marker, content);
+     return marker;
+   }
   addMarker(map) {
 
     let marker = new google.maps.Marker({
       map,
       animation: google.maps.Animation.DROP,
       position: map.getCenter()
-    });
-
-    let content = "<h4>Information!</h4>";
-
-    this.addInfoWindow(marker, content);
-
-  }
-  addMarker2() {
-
-    let marker = new google.maps.Marker({
-      map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: this.map.getCenter()
     });
 
     let content = "<h4>Information!</h4>";
